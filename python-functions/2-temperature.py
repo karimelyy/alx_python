bc448276a3f5fa6472c2cb87f2391413f82a0512
Karim <149385441+karimelyy@users.noreply.github.com>
2023-12-01 16:57:44 +0100

    Update 2-temperature.py

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,3 +1,6 @@
 def convert_to_celsius(fahrenheit):
-    celsius = (fahrenheit - 32) * (5 / 9)
+    if fahrenheit == -459.67:
+        celsius = -273.15
+    else:
+        celsius = (fahrenheit - 32) * (5 / 9)
     return celsius
