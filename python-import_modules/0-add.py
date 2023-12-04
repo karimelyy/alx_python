6accc4812552f9bb21cfe59a7df4c0ed43b399fd
Karim <149385441+karimelyy@users.noreply.github.com>
2023-12-04 15:41:11 +0100

    modules add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 a = 1
 b = 2
-add_0 = __import__('add_0')
-result = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__"
+    add_0 = __import__('add_0')
+    result = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, result))
