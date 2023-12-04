855b89d3936493739cf3f34401eb2627bc53aa48
Karim <149385441+karimelyy@users.noreply.github.com>
2023-12-04 15:53:13 +0100

    module add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 a = 1
 b = 2
 if __name__ == "__main__":
-    import add_0
-    result = add_0.add(a, b)
+    import add_0 as add_module
+    result = add_module.add(a, b)
     print("{} + {} = {}".format(a, b, result))
