08b2d81573a0e9a0f3d99f9e7c0dad51185f934d
Karim <149385441+karimelyy@users.noreply.github.com>
2023-12-05 12:59:40 +0100

    matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -7,3 +7,12 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(num), end=" ")
     if not matrix:
         print()
+#test cases
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer([]) #empty matrix
