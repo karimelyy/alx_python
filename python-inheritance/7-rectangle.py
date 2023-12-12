a6c01372a74134919424eaa211daa33a797858c4
karimelyy <elkarims30@gmail.com>
2023-12-12 16:57:23 +0100

    commit

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,10 +1,49 @@
+"""define an empty class BaseGeometry"""
+class BaseGeometry:
+    """
+    The class is a placeholder for geometry-related classes.
+    Attributes:
+        - No attributes are defined in this base class.
+    Methods:
+        - __init__(self): Constructor method for the BaseGeometry class.
+    """
+    def __init__(self):
+        """
+        Constructor for the BaseGeometry class.
+        Parameters:
+            - None
+        Returns:
+            - None
+        """
+        pass
+    def area(self):
+        """
+        Public instance method that raises an Exception with the message"area() is not implemented"
+        raises:
+            - exception: always raised with the specified message
+        """
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """
+        Public instance method that validates an integer value.
+        Parameters:
+            - name (str): The name of the value.
+            - value: The value to be validated.
+        Raises:
+            - TypeError: If the value is not an integer.
+            - ValueError: If the value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 """
 7-rectangle.py
 Module containing the Rectangle class, which inherits from BaseGeometry.
 Classes:
     - Rectangle: A class representing a rectangle.
 """
-class rectangle(BaseGeometry):
+class Rectangle(BaseGeometry):
     """
     Rectangle class that inherits from BaseGeometry.
     Methods:
