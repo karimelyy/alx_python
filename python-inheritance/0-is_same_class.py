6a3772f47b6ced0294fc71845ed6170b3b324704
karimelyy <elkarims30@gmail.com>
2023-12-12 13:44:31 +0100

    commit

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,2 +1,9 @@
 def is_same_class(obj, a_class):
+    """Check if the object is exactly an instance of the specified class
+    Args:
+        - obj: the object to check
+        - a_class: the class to compare against
+    returns:
+        - true if obj is an instance of a_class; otherwise, false
+    """
     return type(obj) is a_class
