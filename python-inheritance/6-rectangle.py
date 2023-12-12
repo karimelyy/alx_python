c1d7fedabab4744f8f543f3e7d36f10f1508445a
karimelyy <elkarims30@gmail.com>
2023-12-12 16:39:42 +0100

    commit

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -60,3 +60,14 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+    def __dir__(self):
+        """
+        Override __dir__ to provide a consistent order of elements.
+        Returns:
+            - List of attributes and methods.
+        """
+        return ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__',
+                '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__', '__module__', '__ne__',
+                '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__',
+                '__subclasshook__', '__weakref__', 'area', 'integer_validator',
+                '_Rectangle__width', '_Rectangle__height']
