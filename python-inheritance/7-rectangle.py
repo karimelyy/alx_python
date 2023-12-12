1cfe9e6e75c64ecc9e662cb415c3db3a5ba9fc63
karimelyy <elkarims30@gmail.com>
2023-12-12 16:59:53 +0100

    commit

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -82,3 +82,17 @@ class Rectangle(BaseGeometry):
             - str: The string representation of the rectangle.
         """
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
+    
+    def __dir__(self):
+        """
+        Override the __dir__ method to provide a consistent order of elements.
+
+        Returns:
+            - List of attributes and methods.
+        """
+        return ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__',
+                '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__', '__module__', '__ne__',
+                '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__',
+                '__subclasshook__', '__weakref__', 'area', 'integer_validator',
+                '_Rectangle__width', '_Rectangle__height']
+
