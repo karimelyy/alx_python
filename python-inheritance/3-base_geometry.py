85662d3dc08eaaca12ea0212a1f86a4c1b4c7e1d
karimelyy <elkarims30@gmail.com>
2023-12-12 14:55:47 +0100

    commit

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,7 +1,6 @@
 """Define an empty class BaseGeometry"""
 class baseGeometry:
-    """
-    BaseGeometry is an empty class serving as the base class for geometry-related classes.
+    """BaseGeometry is an empty class serving as the base class for geometry-related classes.
 
     Attributes:
     - No attributes are defined in this base class.
@@ -10,7 +9,5 @@ class baseGeometry:
     - No methods are defined in this base class.
     """
     def __init__(self):
-        """
-        This is the constructor for the BaseGeometry class.
-        """
+        """This is the constructor for the BaseGeometry class."""
         pass
