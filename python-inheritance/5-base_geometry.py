072f40eae0c13b58e66e13cdb4615675767425c0
karimelyy <elkarims30@gmail.com>
2023-12-12 16:28:24 +0100

    commit

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -37,3 +37,13 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+        def __dir__(self):
+            """
+            Override __dir__ to provide a consistent order of elements.
+            Returns:
+                - List of attributes and methods.
+            """
+        return ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__',
+                '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__', '__module__', '__ne__',
+                '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__',
+                '__subclasshook__', '__weakref__', 'area', 'integer_validator']
