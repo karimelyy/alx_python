da85e55e0255f0b6b6a287af7580e4ec85235a12
karimelyy <elkarims30@gmail.com>
2023-12-12 15:44:58 +0100

    commit

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,35 @@
+"""define an empty class BaseGeometry"""
+class BaseGeometry:
+    """
+    The class is a placeholder for geometry-related classes.
+    Attributes:
+        - No attributes are defined in this base class.
+    Methods:
+        - __init__(self): Constructor method for the BaseGeometry class.
+    """
+    def __init__(self):
+        """
+        Constructor for the BaseGeometry class.
+        Parameters:
+            - None
+        Returns:
+            - None
+        """
+        pass
+    def __init__(self):
+        """
+        Initialize a new instance of BaseGeometry.
+
+        Parameters:
+        - None
+
+        Returns:
+        - None
+        """
+    def area(self):
+        """
+        Public instance method that raises an Exception with the message"area() is not implemented"
+        raises:
+            - exception: always raised with the specified message
+        """
+        raise Exception("area() is not implemented")
