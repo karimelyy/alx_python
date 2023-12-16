629eb676a47ceadd1bf51a47dc829c2adebe5802
karimelyy <elkarims30@gmail.com>
2023-12-16 18:01:33 +0100

    commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -97,3 +97,8 @@ class Rectangle(Base):
     def area(self):
         """Calculate and return the area of the reactangle"""
         return self.width * self.height
+    
+    def display(self):
+        """Print the reactangle instance using '#' characters"""
+        for _ in range(self.height):
+            print("#" * self.width)
