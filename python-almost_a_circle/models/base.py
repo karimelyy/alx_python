6f4789c054538cdd642e5a2a422e668aaea21726
karimelyy <elkarims30@gmail.com>
2023-12-16 14:36:37 +0100

    commit

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,14 +1,30 @@
 class Base:
+    # Private class attribute to keep track of objects created
     __nb_objects = 0
 
+    # Class constructor
     def __init__(self, id=None):
+        """
+        Initialize a new instance of Base.
+
+        Args:
+            id (int, optional): The ID for the instance. If provided, assigns
+                the given ID; otherwise, increments __nb_objects and assigns
+                the new value to the instance's ID attribute.
+        """
+        # Check if an ID is provided
         if id is not None:
+            # If provided, assign the given ID
             self.id = id
         else:
+            # If not provided, increment __nb_objects and assign the new value
+            # to the instance's ID attribute
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+# Example usage and testing
 if __name__ == "__main__":
+    # Create instances of Base and print their IDs
     b1 = Base()
     print(b1.id)
 
