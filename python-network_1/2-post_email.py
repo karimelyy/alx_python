5a44baa0114ff05d8d083ef18eaf52e3ba2d7c45
karimelyy <elkarims30@gmail.com>
2023-12-18 17:06:42 +0100

    commit

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,10 @@
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {"email": email}
+
+    response = requests.post(url, data=payload)
+    print("Your email is:", response.text)
