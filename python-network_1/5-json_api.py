fe60598fca03e7c544c22cafb537fac77434908c
karimelyy <elkarims30@gmail.com>
2023-12-19 14:40:50 +0100

    commit

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,18 @@
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    payload = {"q": letter}
+
+    response = requests.post(url, data=payload)
+
+    try:
+        json_data = response.json()
+        if json_data:
+            print("[{}] {}".format(json_data['id'], json_data['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
