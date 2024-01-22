8ea94e1749c6fd17d930a4460400c8449a625e2c
karimelyy <elkarims30@gmail.com>
2024-01-22 15:20:57 +0100

    commit

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,14 +1,19 @@
 from flask import Flask
 
-# Create a Flask web application
+# Create a Flask web application instance
 app = Flask(__name__)
 
 # Define a route for the root URL ("/") with strict_slashes=False
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-     """This route displays 'Hello HBNB!' when accessed."""
+    """
+    Route handler for the root URL ("/"). Displays a greeting message.
+
+    Returns:
+        str: The greeting message "Hello HBNB!".
+    """
     return 'Hello HBNB!'
 
-# Run the application on 0.0.0.0 (accessible from any network interface) and port 5000
+# Run the Flask application on 0.0.0.0 (accessible from any network interface) and port 5000
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
