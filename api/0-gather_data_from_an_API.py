55cee89e680e9ab905efb3c980f1f4019fbc32f9
karimelyy <elkarims30@gmail.com>
2024-02-26 18:01:53 +0100

    commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,46 +1,44 @@
 #!/usr/bin/python3
+
 """
-Checks student output for returning info from REST API
+checks student output for returning info from REST API
 """
 
 import requests
 import sys
 
-users_url = "https://jsonplaceholder.typicode.com/users"
-todos_url = "https://jsonplaceholder.typicode.com/todos"
+def get_employee_info(employee_id):
+    # Fetch employee details
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(employee_url)
+    employee_data = response.json()
+    employee_name = employee_data['name']
 
+    # Fetch TODO list for the employee
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    response = requests.get(todos_url)
+    todos_data = response.json()
 
-def first_line_formatting(id):
-    """ Check student output formatting """
+    # Calculate progress
+    total_tasks = len(todos_data)
+    completed_tasks = sum(1 for todo in todos_data if todo['completed'])
 
-    todos_count = 0
-    todos_done = 0
-
-    resp = requests.get(todos_url).json()
-    for i in resp:
-        if i['userId'] == id:
-            todos_count += 1
-        if (i['completed'] and i['userId'] == id):
-            todos_done += 1
-
-    resp = requests.get(users_url).json()
-
-    name = None
-    for i in resp:
-        if i['id'] == id:
-            name = i['name']
-    
-    filename = 'student_output'
-    with open(filename, 'r') as f:
-        first = f.readline().strip()
-
-    output = "Employee {} is done with tasks({}/{}):".format(name, todos_done, todos_count)
-
-    if first == output:
-        print("First line formatting: OK")
-    else:
-        print("First line formatting: Incorrect")
-
+    # Display progress
+    print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
+    for todo in todos_data:
+        if todo['completed']:
+            print(f"\t{todo['title']}")
 
 if __name__ == "__main__":
-    first_line_formatting(int(sys.argv[1]))
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+    
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer")
+        sys.exit(1)
+
+    get_employee_info(employee_id)
+    
