ceb792944f0b4777156c4d07d6721069c9f004f3
karimelyy <elkarims30@gmail.com>
2024-03-07 00:40:08 +0100

    commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,28 +2,28 @@ import requests
 import sys
 
 # Get the employee ID from command line arguments
-employee_id = sys.argv[1]
+id = sys.argv[1]
 
 # Make a GET request to retrieve employee information
-user_request = requests.get('https://jsonplaceholder.typicode.com/users/'+employee_id)
-todos_request = requests.get('https://jsonplaceholder.typicode.com/users/'+employee_id+'/todos')
+request_user = requests.get('https://jsonplaceholder.typicode.com/users/'+employee_id)
+request_todos = requests.get('https://jsonplaceholder.typicode.com/users/'+employee_id+'/todos')
 
 # Parse the JSON response
-user_data = user_request.json()
-todos_data = todos_request.json()
+data_user = request_user.json()
+data_todos = request_todos.json()
 
 # Initialize a counter for completed tasks
-tasks_completed = 0
+completed = 0
 
 # Iterate through the tasks to count completed ones
-for todo in todos_data:
-    if todo.get('completed'):
-        tasks_completed += 1
+for i in data_todos:
+    if i.get('completed'):
+        completed += 1
 
 # Print the employee's name and the number of completed tasks
-print ('Employee "{}" has completed tasks ({}/{}) :'.format(user_data.get('name'), tasks_completed, len(todos_data)))
+print ('Employee "{}" has completed tasks ({}/{}) :'.format(data_user.get('name'), completed, len(data_todos)))
 
 # Print the titles of completed tasks
-for task in todos_data:
+for task in data_todos:
     if task.get('completed'):
         print('\t - ' + task.get('title'))
