ff2f59f06de108785d0f4b652fd9b404057adb71
karimelyy <elkarims30@gmail.com>
2024-03-07 00:49:27 +0100

    commit

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -3,29 +3,27 @@ import csv
 import requests
 import sys
 
-# Extract user ID from command line argument
+# Get user ID from command line argument and convert to string
 user_id = str(sys.argv[1])
 
-# Formulate URLs to request user and todo data
+# Construct URLs to retrieve user and todo data
 request_user = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
 request_todos = "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
 
-# Retrieve user and todo data from JSONPlaceholder API
+# Fetch user and todo data from the JSONPlaceholder API
 data_user = requests.get(request_user).json()
 data_todos = requests.get(request_todos).json()
 
-# Generate filename for CSV based on user ID
+# Define filename for the CSV file based on the user ID
 filename = f"{user_id}.csv"
 
-# Write user and todo data to a CSV file
+# Open the CSV file in write mode and create a CSV writer object
 with open(filename, "w", newline="") as file:
     csvwriter = csv.writer(file, quoting=csv.QUOTE_ALL)
     
-    # Write header row to CSV file
-    csvwriter.writerow(["User ID", "Username", "Task Completed", "Task Title"])
-    
     # Iterate over todo data and write each task to the CSV file
     for task in data_todos:
+        # Write task details to the CSV file
         csvwriter.writerow(
             [user_id, str(data_user["username"]), task["completed"], task["title"]]
         )
